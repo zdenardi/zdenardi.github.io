261950527f9468dbff8ed59a1ef436426ac1c2c9
zachdenardi <zdenardi@gmail.com>
2026-04-09 23:23:43 -0700

    chore:added site field to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   vite: {
+      site: 'https://zdenardi.github.io',
       plugins: [tailwindcss()],
   },
 
